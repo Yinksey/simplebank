0f1bec59c36dba5139e65fe610ee934c1b480e82
yinksey <yinksey.brown@gmail.com>
2026-09-25 16:49:38 +0100

    load env

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+	"github.com/yinksey/simplebank/api"
+	db "github.com/yinksey/simplebank/db/sqlc"
+	"github.com/yinksey/simplebank/util"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("Cannot connect to database:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+}
